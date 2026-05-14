a33a8a5ec5d9ad59804d8b5af1e472455d3fe532
yoralgazin <fistofsnake@gmail.com>
2026-05-14 18:37:00 +0500

    feat: enhance Kafka configuration with producer factory setup

--- src/main/kotlin/org/example/practice/config/KafkaConfig.kt
+++ src/main/kotlin/org/example/practice/config/KafkaConfig.kt
@@ -1,4 +1,14 @@
-package org.example.practice.config 
+package org.example.practice.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 
 @Configuration
 class KafkaConfig {
@@ -6,7 +16,7 @@ class KafkaConfig {
     private lateinit var bootstrapServers: String
 
     @Bean
-    fun ProducerFactory(): ProducerFactory<String, String>{
+    fun ProducerFactory(): ProducerFactory<String, String> {
         val props = mapOf(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringSerializer",
